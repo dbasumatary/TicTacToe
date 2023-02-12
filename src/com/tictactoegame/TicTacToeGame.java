aa4fefdf189dc6b273d79c8b0a5d2086ec295efa
dbasumatary <basudhondip@gmail.com>
2023-02-12 12:24:24 +0530

    Added: Method to create an empty board

--- src/com/tictactoegame/TicTacToeGame.java
+++ src/com/tictactoegame/TicTacToeGame.java
@@ -1,6 +1,13 @@
 package com.tictactoegame;
+/*Creating an empty board*/
 public class TicTacToeGame {
+    static char[] board = new char[10];
+    static void createBoard(){                       //Create a board of size 10 and ignoring 0th index
+        for(int i = 0; i < board.length; i++) {
+            board[i] = ' ';
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to TicTacToe game");
+        createBoard();                        //Calling the method
     }
 }
